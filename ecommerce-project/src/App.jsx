007a78838ee03d13f6a6f984d6acaaf61bb179c9
Ad4rsh <adarshmishra1232580@gmail.com>
2025-10-25 15:08:56 +0530

    List cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,6 @@
 import { HomePage } from './Pages/HomePage'
+import axios from 'axios';
+import { useState , useEffect } from 'react';
 import { Routes, Route } from 'react-router';
 import { CheckoutPage } from './Pages/checkout/CheckoutPage';
 import { TrackingPage } from './Pages/TrackingPage';
@@ -7,11 +9,22 @@ import { OrdersPage } from './Pages/OrdersPage';
 import './App.css'
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data);
+      });
+  },[])
+
+
+
 
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
     </Routes>
